53ac417c6aafddbe7699a52ad0d52c70905c9f0c
Jessica Brush <jessicadawnbrush@gmail.com>
2018-06-24 17:01:41 -0700

    ready to deploy

--- src/App.js
+++ src/App.js
@@ -2,17 +2,64 @@ import React, { Component } from "react";
 import FriendCard from "./components/FriendCard";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
+import Navbar from "./components/Navbar";
 import friends from "./friends.json";
 import "./App.css";
+
+let shuffleArr = require('shuffle-array');
 
 class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
-    friends
+    friends,
+    currentScore: 0,
+    highScore: 0
   };
 
-  removeFriend = id => {
-    // Filter this.state.friends for friends with an id not equal to the id being removed
+  shuffle = () => {
+    let friends = this.state.friends;
+    shuffleArr(friends);
+    this.setState({ friends });
+  }
+
+  clicked = (id) => {
+    let friends = this.state.friends;
+    friends.forEach( elem => {
+      if (elem.id === id) {
+        
+        if (elem.clicked === false) {
+          
+          elem.clicked = true;
+          console.log(elem);
+          this.state.currentScore += 1;
+          if (this.state.highScore < this.state.currentScore) {
+            this.state.highScore = this.state.currentScore;
+          }
+        } 
+        else {
+          this.endGame();
+        }
+    };
+  });
+    console.log(this.state.currentScore);
+    this.shuffle();
+  }
+
+  endGame = () => {
+    this.state.currentScore = 0;
+    this.shuffle();
+    friends.forEach( creature => {
+    creature.clicked = false; 
+    })
+    console.log(this.state.currentScore);
+    console.log(friends);
+  }
+
+// store a  
+// write a function that shuffles the array, defining a method within the class....
+
+  storeFriend = id => {
+    // Filter this.state.friends for friends with an id equal to the id being removed
     const friends = this.state.friends.filter(friend => friend.id !== id);
     // Set this.state.friends equal to the new friends array
     this.setState({ friends });
@@ -21,20 +68,23 @@ class App extends Component {
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
-      <Wrapper>
-        <Title>Friends List</Title>
-        {this.state.friends.map(friend => (
-          <FriendCard
-            removeFriend={this.removeFriend}
-            id={friend.id}
-            key={friend.id}
-            name={friend.name}
-            image={friend.image}
-            occupation={friend.occupation}
-            location={friend.location}
-          />
-        ))}
-      </Wrapper>
+      <div>
+        <Navbar currentScore={this.state.currentScore} highScore={this.state.highScore}/>
+
+      
+        <Wrapper>
+          <Title>Friends List</Title>
+          {this.state.friends.map(friend => (
+            <FriendCard
+              storeFriend={this.storeFriend}
+              id={friend.id}
+              key={friend.id}
+              clicked={this.clicked}
+              image={friend.image}
+            />
+          ))}
+        </Wrapper>
+      </div>
     );
   }
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,22 @@
+// Untouched bootstrap nav code
+import React from "react";
+import "./Navbar.css";
+
+
+const Navbar = props => (
+    // <div className="navbar">{props.children}>
+
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="/"> {props.currentScore} {props.highScore}</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+
+        </div>
+    </nav>
+
+
+);
+
+export default Navbar;
